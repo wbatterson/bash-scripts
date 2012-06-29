f204bde36f2f987db8c0ee35db1aae90c6efc631
wbatterson <wbatterson@github.com>
2012-06-28 17:48:51 -0700

    Skynet Monitor: Restart IIS Script if curl fails to return status 1

---
+++ restartiis.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash -x
+for h in `cat chkurl.lst`; do
+   export CHK=`curl -s -m 30 $h|grep -c "Col1=hello333"`;
+   if [ "$CHK" != 1 ]; then
+      s=`echo $h|awk -F/ '{print $3}'`;
+      echo "`date` : $s has been reset" >> /home/abraham77/logs/`date +%Y%m%d`.log
+      /cygdrive/c/WINDOWS/system32/iisreset $s
+   fi
+   sleep 2
+   s=`echo $h|awk -F/ '{print $3}'`;
+   /cygdrive/c/WINDOWS/system32/iisreset /status $s|grep "Stopped" && /cygdrive/c/WINDOWS/system32/iisreset $s;
+done
