0622603262cbea0aa8e1a7f9539cfe7fffdcf569
wbatterson <wbatterson@gmail.com>
2012-12-10 12:26:23 -0700

    Update restartiis.sh

--- restartiis.sh
+++ restartiis.sh
@@ -3,7 +3,7 @@ for h in `cat chkurl.lst`; do
    export CHK=`curl -s -m 30 $h|grep -c "Col1=hello333"`;
    if [ "$CHK" != 1 ]; then
       s=`echo $h|awk -F/ '{print $3}'`;
-      echo "`date` : $s has been reset" >> /home/abraham77/logs/`date +%Y%m%d`.log
+      echo "`date` : $s has been reset" >> /home/admin/logs/`date +%Y%m%d`.log
       /cygdrive/c/WINDOWS/system32/iisreset $s
    fi
    sleep 2
